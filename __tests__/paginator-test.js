ce4231cca51d4ed3c791743324a519968c49337c
Juho Vepsalainen <bebraw@gmail.com>
2015-01-21 15:42:10 +0200

    Add initial tests for paginator
    Related to #3.

--- __tests__/paginator-test.js
+++ __tests__/paginator-test.js
@@ -9,5 +9,42 @@ var Paginator = require('../lib/paginator.jsx');
 
 
 describe('Paginator', function() {
+    it('should construct a link for each page', function() {
+        var amount = 5;
+        var paginator = TestUtils.renderIntoDocument(
+            <Paginator pages={amount} />
+        );
 
+        var links = TestUtils.scryRenderedDOMComponentsWithTag(
+            paginator, 'a');
+
+        expect(links.length).toEqual(amount);
+    });
+
+    it('should mark the selected page', function() {
+        var amount = 5;
+        var paginator = TestUtils.renderIntoDocument(
+            <Paginator pages={amount} page={1} />
+        );
+
+        var selected = TestUtils.scryRenderedDOMComponentsWithClass(
+            paginator, 'selected');
+
+        expect(selected.length).toEqual(1);
+    });
+
+    it('should trigger handler on select', function() {
+        var selectIndex = 2;
+        var select = function(i) {
+            expect(i).toEqual(selectIndex);
+        };
+        var paginator = TestUtils.renderIntoDocument(
+            <Paginator pages={5} onSelect={select} />
+        );
+
+        var links = TestUtils.scryRenderedDOMComponentsWithTag(
+            paginator, 'a');
+
+        TestUtils.Simulate.click(links[selectIndex]);
+    });
 });
