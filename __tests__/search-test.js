22dce0ca7c3317d466127f21d68f8c14331fe12e
Juho Vepsalainen <bebraw@gmail.com>
2015-01-21 14:33:43 +0200

    Add initial tests for search
    Related to #3.

--- __tests__/search-test.js
+++ __tests__/search-test.js
@@ -9,5 +9,42 @@ var Search = require('../lib/search.jsx');
 
 
 describe('Search', function() {
+    it('should have a dropdown with default `all` option', function() {
+        var search = TestUtils.renderIntoDocument(
+            <Search />
+        );
 
+        var options = TestUtils.scryRenderedDOMComponentsWithTag(
+            search, 'option');
+
+        expect(options.length).toEqual(1);
+        expect(options[0].getDOMNode().value).toEqual('all');
+    });
+
+    it('should have a dropdown that contain columns that have both property and header', function() {
+        var columns = [
+            {
+                property: 'first',
+                header: 'First'
+            },
+            {
+                property: 'second'
+            },
+            {
+                header: 'Third'
+            }
+        ];
+
+        var search = TestUtils.renderIntoDocument(
+            <Search columns={columns} />
+        );
+
+        var options = TestUtils.scryRenderedDOMComponentsWithTag(
+            search, 'option');
+
+        expect(options.length).toEqual(2);
+        expect(options[0].getDOMNode().value).toEqual('all');
+        expect(options[1].getDOMNode().value).toEqual(columns[0].property);
+        expect(options[1].getDOMNode().textContent).toEqual(columns[0].header);
+    });
 });
