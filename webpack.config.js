138a33277c2a699f160794a050bb014e4d0a1056
Juho Vepsalainen <bebraw@gmail.com>
2015-01-22 09:16:02 +0200

    Improve formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,45 @@
+'use strict';
 var webpack = require('webpack');
 
+
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://0.0.0.0:3000',
-    'webpack/hot/only-dev-server',
-    './demos/index'
-  ],
-  output: {
-    path: __dirname,
-    filename: 'bundle.js',
-    publicPath: '/demos/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  resolve: {
-    extensions: ['', '.js', '.jsx', '.css', '.png', '.jpg']
-  },
-  module: {
-    loaders: [
-      { test: /\.(js|jsx)$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ },
-      { test: /\.css$/, loaders: ['style', 'css'] },
-      { test: /\.png$/, loader: 'url-loader?limit=100000&mimetype=image/png' },
-      { test: /\.jpg$/, loader: 'file-loader' },
-    ]
-  }
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:3000',
+        'webpack/hot/only-dev-server',
+        './demos/index'
+    ],
+    output: {
+        path: __dirname,
+        filename: 'bundle.js',
+        publicPath: '/demos/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.css', '.png', '.jpg']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(js|jsx)$/,
+                loaders: ['react-hot', 'jsx?harmony'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css']
+            },
+            {
+                test: /\.png$/,
+                loader: 'url-loader?limit=100000&mimetype=image/png'
+            },
+            {
+                test: /\.jpg$/,
+                loader: 'file-loader'
+            },
+        ]
+    }
 };
