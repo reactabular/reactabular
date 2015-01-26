ca143eba4b3fddc12bb98d1ff51a8c20f255ee7d
Juho Vepsalainen <bebraw@gmail.com>
2015-01-26 11:22:01 +0200

    Improve formatting

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+'use strict';
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
+
 var config = require('./config/webpack.config');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true
-}).listen(3000, '0.0.0.0', function (err, result) {
-  if (err) {
-    console.log(err);
-  }
 
-  console.log('Listening at 0.0.0.0:3000');
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true
+}).listen(3000, '0.0.0.0', function (err, result) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log('Listening at 0.0.0.0:3000');
 });
