2ef6f3666f586da72a0f76d2b6641f9acd501ac7
Juho Vepsalainen <bebraw@gmail.com>
2015-01-28 12:27:05 +0200

    Push gh fork to a separate component
    This probably should be pushed to a separate project.

--- demos/app.js
+++ demos/app.js
@@ -6,6 +6,7 @@ require('../css/style.css');
 
 var React = require('react');
 
+var Fork = require('./fork.jsx');
 var FullTable = require('./full_table.jsx');
 var EditorsTable = require('./editors_table.jsx');
 
@@ -15,13 +16,7 @@ var readme = require('../README.md');
 module.exports = React.createClass({
     render() {
         return <div className='pure-g'>
-            <a href='https://github.com/bebraw/reactabular'>
-                <img
-                    className='github-fork'
-                    src='https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67'
-                    alt='Fork me on GitHub'
-                    data-canonical-src='https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png'></img>
-            </a>
+            <Fork project='bebraw/reactabular'></Fork>
             <header className='pure-u-1'>
                 <h1>Reactabular</h1>
 
