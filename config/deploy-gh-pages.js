dc75e15db9f6e893177f7e3e08920edac7bf8d46
Juho Vepsalainen <bebraw@gmail.com>
2015-01-28 12:38:23 +0200

    Tidy up

--- config/deploy-gh-pages.js
+++ config/deploy-gh-pages.js
@@ -8,7 +8,5 @@ var config = require('./webpack.gh-pages');
 main();
 
 function main() {
-    var path = config.output.path;
-
     ghpages.publish(config.output.path, console.error.bind(console));
 }
