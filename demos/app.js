def002c492ccea8b9ff5bd8e199f6200fe401347
Juho Vepsalainen <bebraw@gmail.com>
2015-02-03 17:18:29 +0200

    Move style imports to `index.js`

--- demos/app.js
+++ demos/app.js
@@ -1,13 +1,5 @@
 'use strict';
 
-require('purecss/pure.css');
-require('highlight.js/styles/github.css');
-require('react-ghfork/gh-fork-ribbon.ie.css');
-require('react-ghfork/gh-fork-ribbon.css');
-require('react-pagify/style.css');
-require('./main.css');
-require('../style.css');
-
 var React = require('react');
 var Fork = require('react-ghfork');
 
--- demos/index.js
+++ demos/index.js
@@ -1,6 +1,15 @@
 'use strict';
 
+require('purecss/pure.css');
+require('highlight.js/styles/github.css');
+require('react-ghfork/gh-fork-ribbon.ie.css');
+require('react-ghfork/gh-fork-ribbon.css');
+require('react-pagify/style.css');
+require('./main.css');
+require('../style.css');
+
 var React = require('react'),
     App = require('./app');
+
 
 React.render(<App />, document.body);
