32eac77e1c72e4c519a399ae7aff968a6d82ee40
Juho Vepsalainen <bebraw@gmail.com>
2015-02-27 11:59:04 +0200

    Use `react/addons` everywhere within `/lib`
    This should help with Travis issues.

--- lib/cells/edit.jsx
+++ lib/cells/edit.jsx
@@ -1,6 +1,6 @@
 'use strict';
 
-var React = require('react');
+var React = require('react/addons');
 
 
 module.exports = function(o) {
--- lib/search.jsx
+++ lib/search.jsx
@@ -1,6 +1,6 @@
 'use strict';
 
-var React = require('react');
+var React = require('react/addons');
 var formatters = require('./formatters');
 
 
