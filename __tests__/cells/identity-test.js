599c791a99b43a9d0f348683aaaaa6a0500ec76c
Juho Vepsalainen <bebraw@gmail.com>
2015-03-03 15:42:17 +0200

    Fix identity test
    Related to #13.

--- __tests__/cells/identity-test.js
+++ __tests__/cells/identity-test.js
@@ -10,6 +10,11 @@ var identity = require('../../lib/cells/identity.js');
 describe('identity', function(){
     it('formats correctly', function() {
         var value = "never odd or even";
-        expect(identity(value)).toEqual({value: value});
+        var formatted = "Never odd or even";
+
+        expect(identity({
+            original: value,
+            formatted: formatted
+        })).toEqual({value: formatted});
     });
 });
