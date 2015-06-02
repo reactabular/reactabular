9656e8b0e0ce4d3324e9d911c97261769284dc76
Juho Vepsalainen <bebraw@gmail.com>
2015-06-02 16:47:43 +0300

    Make webpack use colors at output

--- dev-server/server.js
+++ dev-server/server.js
@@ -5,14 +5,20 @@ var WebpackDevServer = require('webpack-dev-server');
 var config = require('../webpack.config');
 
 
+var ip = '0.0.0.0';
+var port = 3000;
 new WebpackDevServer(webpack(config), {
     contentBase: __dirname,
     publicPath: config.output.publicPath,
-    hot: true
-}).listen(3000, '0.0.0.0', function (err) {
+    hot: true,
+    historyApiFallback: true,
+    stats: {
+        colors: true
+    },
+}).listen(port, ip, function (err) {
     if(err) {
         return console.log(err);
     }
 
-    console.log('Listening at 0.0.0.0:3000');
+    console.log('Listening at ' + ip + ':' + port);
 });
