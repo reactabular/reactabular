02e1238939f08271beceaafad366427bc7d9a10f
Cameron Tangney <ctangney@u.washington.edu>
2015-07-14 00:44:35 -0700

    Simplifies the cell rendering API. If a cell function is defined, call it on the raw value. If the returned value is a (plain) object, pass that through. Otherwise, just give the cell the value.

--- src/table.js
+++ src/table.js
@@ -71,35 +71,18 @@ module.exports = React.createClass({
                         columns.map((column, j) => {
                             var property = column.property;
                             var value = row[property];
-                            var cell = column.cell || [formatters.identity];
-                            var content;
+                            if (isFunction(column.cell)) {
+                              // Always replace value with the result of cell()
+                              value = column.cell(value, data, i, property);
+                            }
 
-                            cell = isFunction(cell) ? [cell] : cell;
+                            var content = {value: value};
 
-                            content = reduce([value].concat(cell), (v, fn) => {
-                                if(v && React.isValidElement(v.value)) {
-                                    return v;
-                                }
-
-                                if(isPlainObject(v)) {
-                                    return merge(v, {
-                                        value: fn(v.value, data, i, property)
-                                    });
-                                }
-
-                                var val = fn(v, data, i, property);
-
-                                if(val && !isUndefined(val.value)) {
-                                    return val;
-                                }
-
-                                // formatter shortcut
-                                return {
-                                    value: val
-                                };
-                            });
-
-                            content = content || {};
+                            if (isPlainObject(value)) {
+                              // If a plain object was returned, we pass along its keys/values
+                              // to the table cell (e.g. props, value, className, etc.)
+                              content = merge(content, value);
+                            }
 
                             return <td key={j + '-cell'} {...content.props}>{content.value}</td>;
                         }
