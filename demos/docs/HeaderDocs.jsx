51eaa32a0b8e37ea76f99681047f235a9f5145d0
Juho Vepsalainen <bebraw@gmail.com>
2016-06-20 14:12:08 +0300

    docs - Add an example showing how to customize header

--- demos/docs/HeaderDocs.jsx
+++ demos/docs/HeaderDocs.jsx
@@ -1,8 +1,12 @@
+/* eslint-ignore no-console */
 import React from 'react';
 import { Page, ReactSpecimen } from 'catalog';
 import {
   Table,
 } from '../../src';
+import {
+  ColumnFilters,
+} from '../components';
 
 const data = [
   {
@@ -45,5 +49,29 @@ export default () => (
         <Table.Header />
       </Table>
     </ReactSpecimen>
+
+    <h2>Customizing <code>Table.Header</code></h2>
+
+    <p>
+      It is possible to customize a header by passing child components to it. This way you can implement filtering per column for instance.
+    </p>
+
+    <ReactSpecimen span={6}>
+      <Table
+        className="pure-table pure-table-striped"
+        columns={columns}
+        data={data}
+      >
+        <Table.Header>
+          <ColumnFilters
+            columns={columns}
+            onChange={value => console.log('new value', value)}
+          />
+        </Table.Header>
+
+        <Table.Body />
+      </Table>
+    </ReactSpecimen>
+
   </Page>
 );
