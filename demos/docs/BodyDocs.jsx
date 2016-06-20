dcd68afca3444d6c1bdffd3ea692d7ee4003c4ec
Juho Vepsalainen <bebraw@gmail.com>
2016-06-20 14:07:12 +0300

    docs - Add a table body example

--- demos/docs/BodyDocs.jsx
+++ demos/docs/BodyDocs.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
 import { Page, ReactSpecimen } from 'catalog';
+import {
+  Table,
+} from '../../src';
+
+const data = [
+  {
+    id: 100,
+    name: 'Adam',
+  },
+  {
+    id: 101,
+    name: 'Brian',
+  },
+];
+
+const columns = [
+  {
+    property: 'name',
+    header: 'Name',
+  },
+];
 
 export default () => (
   <Page>
-    <h2>Body</h2>
+    <h2><code>Table.Body</code></h2>
 
-    <p>TODO</p>
+    <p>
+      <code>Table.Body</code> renders a table data within a <code>Table</code> context.
+    </p>
 
     <hr />
 
-    <ReactSpecimen span={3}>
-      <div>demo</div>
+    <ReactSpecimen span={6}>
+      <Table
+        className="pure-table pure-table-striped"
+        columns={columns}
+        data={data}
+      >
+        <Table.Header />
+
+        <Table.Body />
+
+        <Table.Header />
+
+        <Table.Body />
+      </Table>
     </ReactSpecimen>
   </Page>
 );
