afa1c7c48ed30b0178c5c5fbaf4f86108cae9682
Juho Vepsalainen <bebraw@gmail.com>
2016-06-22 13:55:40 +0300

    formatters - Add initial tests for the highlight formatter

---
+++ tests/formatters_test.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import { formatters } from '../src';
+import { expect } from 'chai';
+
+const highlight = formatters.highlight;
+
+describe('highlight', function () {
+  it('should not highlight if there is no match at all', function () {
+    const value = 'foobar';
+    const highlighter = highlight(() => []);
+    const result = TestUtils.renderIntoDocument(<Wrapper>{highlighter(value)}</Wrapper>);
+    const searchResult = TestUtils.findRenderedDOMComponentWithClass(
+      result, 'search-result'
+    );
+
+    expect(searchResult.children[0].innerHTML).to.equal(value);
+  });
+
+  it('should highlight matching portion', function () {
+    const value = 'foobar';
+    const highlighter = highlight(() => [{
+      startIndex: 0,
+      length: 2,
+    }]);
+    const result = TestUtils.renderIntoDocument(<Wrapper>{highlighter(value)}</Wrapper>);
+    const highlightResult = TestUtils.findRenderedDOMComponentWithClass(
+      result, 'highlight'
+    );
+
+    expect(highlightResult.innerHTML).to.equal(value.slice(0, 2));
+  });
+
+  it('should highlight whole if there is a full match', function () {
+    const value = 'foobar';
+    const highlighter = highlight(() => [{
+      startIndex: 0,
+      length: value.length,
+    }]);
+    const result = TestUtils.renderIntoDocument(<Wrapper>{highlighter(value)}</Wrapper>);
+    const highlightResult = TestUtils.findRenderedDOMComponentWithClass(
+      result, 'highlight'
+    );
+
+    expect(highlightResult.innerHTML).to.equal(value);
+  });
+});
+
+class Wrapper extends React.Component { // eslint-disable-line max-len, react/prefer-stateless-function
+  render() {
+    return <div>{this.props.children}</div>;
+  }
+}
