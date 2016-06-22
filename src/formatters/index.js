5c8c82b9a403b3c4f619fda6089f55e6886de783
Juho Vepsalainen <bebraw@gmail.com>
2016-06-22 13:34:39 +0300

    formatters - Drop unused formatters (identity, lowercase)
    Smaller API surface this way. Those two were too trivial. Highlight is
    the only interesting one.

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,6 +1,4 @@
 /* eslint-disable global-require */
 module.exports = {
   highlight: require('./highlight').default,
-  identity: require('./identity').default,
-  lowercase: require('./lowercase').default,
 };
