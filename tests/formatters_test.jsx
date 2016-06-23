baf0db10e4e4cbf54772cc65143b9d78cbb87652
Juho Vepsalainen <bebraw@gmail.com>
2016-06-23 16:49:22 +0300

    formatters - Test that it highlights from the middle

--- tests/formatters_test.jsx
+++ tests/formatters_test.jsx
@@ -32,6 +32,20 @@ describe('highlight', function () {
     expect(highlightResult.innerHTML).to.equal(value.slice(0, 2));
   });
 
+  it('should highlight from the middle', function () {
+    const value = 'foobar';
+    const highlighter = highlight(() => [{
+      startIndex: 2,
+      length: 4,
+    }]);
+    const result = TestUtils.renderIntoDocument(<Wrapper>{highlighter(value)}</Wrapper>);
+    const highlightResult = TestUtils.findRenderedDOMComponentWithClass(
+      result, 'highlight'
+    );
+
+    expect(highlightResult.innerHTML).to.equal(value.slice(2));
+  });
+
   it('should highlight whole if there is a full match', function () {
     const value = 'foobar';
     const highlighter = highlight(() => [{
