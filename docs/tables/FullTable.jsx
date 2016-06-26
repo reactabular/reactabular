2dc59b0e2eb4b0bcbc100e7aa784a62b53ac755b
Juho Vepsalainen <bebraw@gmail.com>
2016-06-26 15:29:56 +0300

    docs - Push `paginate` to `FullTable`
    Neater this way.

--- docs/tables/FullTable.jsx
+++ docs/tables/FullTable.jsx
@@ -17,7 +17,6 @@ import {
   Paginator, PrimaryControls,
 } from '../helpers';
 import countries from '../data/countries';
-import { paginate } from '../common';
 
 const schema = {
   type: 'object',
@@ -323,6 +322,21 @@ class FullTable extends React.Component {
       data: this.state.data,
     });
   }
+}
+
+function paginate(data = [], o) {
+    // adapt to zero indexed logic
+  const page = o.page - 1 || 0;
+  const perPage = o.perPage;
+
+  const amountOfPages = Math.ceil(data.length / perPage);
+  const startPage = page < amountOfPages ? page : 0;
+
+  return {
+    amount: amountOfPages,
+    data: data.slice(startPage * perPage, startPage * perPage + perPage),
+    page: startPage,
+  };
 }
 
 export default FullTable;
