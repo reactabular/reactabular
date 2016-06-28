4528eac67bfbd455d919710201c03c98981f96c4
Juho Vepsalainen <bebraw@gmail.com>
2016-06-28 21:06:51 +0300

    docs - Add a small evaluator to the Excel example
    This actually works now. There's `eval` but I didn't feel like
    reimplementing that. Resolving cell values was enough work.

--- docs/tables/ExcelTable.jsx
+++ docs/tables/ExcelTable.jsx
@@ -22,7 +22,7 @@ export default class ExcelTable extends React.Component {
 
       // Capture the value when the user has finished
       onValue: ({ value, columnIndex, rowIndex }) => {
-        this.state.data[rowIndex][columnIndex] = value;
+        this.state.data[rowIndex][columnIndex - 1] = value;
 
         this.setState({
           editedCell: null,
@@ -85,7 +85,7 @@ export default class ExcelTable extends React.Component {
         }
       ],
       data: [
-        ['123', '234', '11', '44'],
+        ['123', '234', '=A1 + B1', '44'],
         ['11', '1', '5', '56'],
         ['3', '4', '=A1', '88'],
         ['33', '114', '150', '77']
@@ -108,15 +108,53 @@ export default class ExcelTable extends React.Component {
 function evaluator(getData) {
   const characters = 'ABCD';
 
+  const resolveInput = (data, input, level = 0) => {
+    // Recursion trap
+    if (level > 10) {
+      return input;
+    }
+
+    return input.
+      trim().
+      slice(1).
+      split(' ').
+      map(
+        c => {
+          const reference = resolveReference(data, c);
+
+          if (reference[0] === '=') {
+            return `(${resolveInput(data, reference.slice(1), level + 1)})`;
+          }
+
+          return reference;
+        }
+      ).
+      join(' ');
+  };
+
+  const resolveReference = (data, input) => {
+    const x = characters.indexOf(input[0]); // Supports just one character
+    const y = parseInt(input[1] - 1, 10); // Supports just one number
+
+    if (x >= 0 && y < data[y].length) {
+      return data[y][x];
+    }
+
+    return input;
+  };
+
   return input => {
     const data = getData();
 
     if (input[0] === '=') {
-      const x = characters.indexOf(input[1]); // Supports just one character
-      const y = parseInt(input[2] - 1, 10); // Supports just one number
+      const result = resolveInput(data, input);
 
-      if (x >= 0 && y < data[x].length) {
-        return <span style={{ fontWeight: 'bold' }}>{data[x][y]}</span>;
+      if (result) {
+        try {
+          return <span style={{ fontWeight: 'bold' }}>{eval(result)}</span>; // eslint-disable-line no-eval, max-len
+        } catch (err) {
+          console.error(err); // eslint-disable-line no-console
+        }
       }
 
       return <span style={{ color: 'red' }}>!ERR</span>;
