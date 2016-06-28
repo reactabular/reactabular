3845797e11ba97503603029032c4735f69cb5f92
Juho Vepsalainen <bebraw@gmail.com>
2016-06-28 20:28:38 +0300

    docs - Evaluate `=A1` kind of statements at the excel example

--- docs/tables/ExcelTable.jsx
+++ docs/tables/ExcelTable.jsx
@@ -30,17 +30,27 @@ export default class ExcelTable extends React.Component {
         });
       }
     });
+    const evaluate = evaluator(() => this.state.data);
 
     this.state = {
       editedCell: null, // Track the edited cell somehow
       columns: [
         {
           header: {
+            label: ''
+          },
+          cell: {
+            format: (value, { rowIndex }) => rowIndex + 1
+          }
+        },
+        {
+          header: {
             label: 'A'
           },
           cell: {
             property: 0,
-            transforms: [editable(editors.input())]
+            transforms: [editable(editors.input())],
+            format: evaluate
           }
         },
         {
@@ -49,7 +59,8 @@ export default class ExcelTable extends React.Component {
           },
           cell: {
             property: 1,
-            transforms: [editable(editors.input())]
+            transforms: [editable(editors.input())],
+            format: evaluate
           }
         },
         {
@@ -58,7 +69,8 @@ export default class ExcelTable extends React.Component {
           },
           cell: {
             property: 2,
-            transforms: [editable(editors.input())]
+            transforms: [editable(editors.input())],
+            format: evaluate
           }
         },
         {
@@ -67,14 +79,15 @@ export default class ExcelTable extends React.Component {
           },
           cell: {
             property: 3,
-            transforms: [editable(editors.input())]
+            transforms: [editable(editors.input())],
+            format: evaluate
           }
         }
       ],
       data: [
         ['123', '234', '11', '44'],
         ['11', '1', '5', '56'],
-        ['3', '4', '50', '88'],
+        ['3', '4', '=A1', '88'],
         ['33', '114', '150', '77']
       ]
     };
@@ -90,4 +103,25 @@ export default class ExcelTable extends React.Component {
       </Table>
     );
   }
+}
+
+function evaluator(getData) {
+  const characters = 'ABCD';
+
+  return input => {
+    const data = getData();
+
+    if (input[0] === '=') {
+      const x = characters.indexOf(input[1]); // Supports just one character
+      const y = parseInt(input[2] - 1, 10); // Supports just one number
+
+      if (x >= 0 && y < data[x].length) {
+        return <span style={{ fontWeight: 'bold' }}>{data[x][y]}</span>;
+      }
+
+      return <span style={{ color: 'red' }}>!ERR</span>;
+    }
+
+    return input;
+  };
 }
