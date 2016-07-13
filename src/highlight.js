f1a1b5380e06084f805961e94cdc7e4ad6a2c686
Juho Vepsalainen <bebraw@gmail.com>
2016-07-13 14:42:26 +0300

    highlight - Support `column.cell.resolve`
    This logic should go to some utility that can be reused given it's the
    same as for search. An alternative would be to find better composition
    (another filter?).

--- src/highlight.js
+++ src/highlight.js
@@ -1,19 +1,29 @@
-function highlight({ matches, query }) {
+function highlight({ columns, matches, query }) {
   return rows => rows.map(row => {
     const ret = {
       _highlights: {}
     };
 
-    Object.keys(row).forEach(key => {
-      const value = row[key];
+    columns.forEach(column => {
+      // XXX: same resolver as for search -> reuse
+      const property = column.cell.property;
+      const value = row[property];
+      const resolver = column.cell.resolve || (a => a);
+      let resolvedValue = resolver(value, { rowData: row, property });
 
-      ret[key] = value;
+      if (typeof resolvedValue === 'undefined' || resolvedValue === null) {
+        resolvedValue = '';
+      }
+
+      resolvedValue = resolvedValue.toString ? resolvedValue.toString() : '';
+
+      ret[property] = row[property];
 
       // Stash highlighted value based on index
       // so it can be extracted later for highlighting
-      ret._highlights[key] = matches({ // eslint-disable-line no-underscore-dangle
-        value,
-        query: query[key] || query.all
+      ret._highlights[property] = matches({ // eslint-disable-line no-underscore-dangle
+        value: resolvedValue,
+        query: query[property] || query.all
       });
     });
 
