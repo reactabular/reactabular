985722c3aebe238f5c59f033415136c06c590be6
Juho Vepsalainen <bebraw@gmail.com>
2016-07-14 17:06:49 +0300

    docs - Add Sticky helper

---
+++ docs/helpers/Sticky.jsx
@@ -0,0 +1,54 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import { Table } from '../../src';
+
+// Ref -> class
+class Header extends React.Component {
+  render() {
+    const { style, onScroll, ...props } = this.props;
+
+    return (
+      <Table.Header
+        style={{
+          ...style || {},
+          display: 'block',
+          overflow: 'auto'
+        }}
+        {...props}
+        onScroll={({ target: { scrollLeft } }) => onScroll(scrollLeft)}
+      />
+    );
+  }
+}
+Header.propTypes = {
+  style: React.PropTypes.any,
+  onScroll: React.PropTypes.func
+};
+
+// Ref -> class
+class Body extends React.Component {
+  render() {
+    const { style, onScroll, ...props } = this.props;
+
+    return (
+      <Table.Body
+        style={{
+          ...style || {},
+          display: 'block',
+          overflow: 'auto'
+        }}
+        {...props}
+        onScroll={({ target: { scrollLeft } }) => onScroll(scrollLeft)}
+      />
+    );
+  }
+}
+Body.propTypes = {
+  style: React.PropTypes.any,
+  onScroll: React.PropTypes.func
+};
+
+export default {
+  Header,
+  Body
+};
