9f3f1d65fd7d5c2af065883b5d709de1d22b3424
Juho Vepsalainen <bebraw@gmail.com>
2016-07-22 15:33:16 +0300

    reactabular - Fix adapter export interface
    Given each package exports an object, this needs to use the same idea.

--- packages/reactabular/index.js
+++ packages/reactabular/index.js
@@ -1,6 +1,15 @@
-export { default as Table } from '../reactabular-table';
-export { default as search } from '../reactabular-search';
-export { default as sort } from '../reactabular-sort';
-export { default as edit } from '../reactabular-edit';
-export { default as highlight } from '../reactabular-highlight';
-export { default as resolve } from '../reactabular-resolve';
+import * as Table from '../reactabular-table';
+import * as search from '../reactabular-search';
+import * as sort from '../reactabular-sort';
+import * as edit from '../reactabular-edit';
+import * as highlight from '../reactabular-highlight';
+import * as resolve from '../reactabular-resolve';
+
+export {
+  Table,
+  search,
+  sort,
+  edit,
+  highlight,
+  resolve
+};
