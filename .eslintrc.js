6b21047c90383b452014e708a9a8df22a5622280
Juho Vepsalainen <bebraw@gmail.com>
2016-07-22 17:44:23 +0300

    eslint - Disable import resolution check
    The webpack aliases break this. It would be nice to figure out a good
    fix.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "extends": "airbnb",
   "parser": "babel-eslint",
   "env": {
@@ -13,10 +13,11 @@
     "comma-dangle": ["error", "never"],
     "prefer-arrow-callback": 0,
     "func-names": 0,
+    "import/no-unresolved": 0, // XXX: Figure out how to handle webpack aliases
     "no-underscore-dangle": 0,
     "no-unused-expressions": 0,
     "no-use-before-define": 0,
     "react/sort-comp": 0,
     "react/no-multi-comp": 0
   }
-}
+};
