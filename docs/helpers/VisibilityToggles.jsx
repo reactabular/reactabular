c482e06f8d9c62bb67ba2ec8c408e2d3bd303190
Juho Vepsalainen <bebraw@gmail.com>
2016-08-12 14:53:38 +0300

    docs - Improve visibility toggles
    * Added support for `styles`.
    * Accept custom `props`.
    * Improved class naming.

--- docs/helpers/VisibilityToggles.jsx
+++ docs/helpers/VisibilityToggles.jsx
@@ -1,39 +1,70 @@
 import React from 'react';
-import uuid from 'uuid';
 
-const VisibilityToggles = ({ columns, onToggleColumn }) => {
-  const labelId = `label-${uuid.v4()}`;
+const defaultStyles = {
+  container: {
+    backgroundColor: '#ddd', padding: '1em', margin: '1em' // TODO: push to style.css
+  },
+  label: {
+    marginRight: '1em' // TODO: push to style.css
+  },
+  toggle: {
+    marginLeft: '0.5em' // TODO: push to style.css
+  }
+};
+
+const VisibilityToggles = ({
+  columns,
+  onToggleColumn,
+  styles = {},
+  ...props
+}) => {
+  const visibilityStyles = {
+    ...defaultStyles,
+    ...styles
+  };
 
   return (
     <div
-      className="visibility-toggles"
-      style={{ backgroundColor: '#ddd', padding: '1em', margin: '1em' }}
+      className="visibility-container"
+      style={visibilityStyles.container}
+      {...props}
     >
       {columns.filter(column => column.header).map(
-        ({ header: { label }, visible }, columnIndex) => (
-          <label
-            className="visibility-toggle"
-            htmlFor={labelId}
-            style={{ marginRight: '1em' }}
-            key={`visibility-toggle-${columnIndex}`}
-          >
-            <span>{label}</span>
-            <input
-              type="checkbox"
-              id={labelId}
-              style={{ marginLeft: '0.5em' }}
-              checked={visible}
-              onChange={() => onToggleColumn(columnIndex)}
-            />
-          </label>
-        )
+        ({ header: { label }, visible }, columnIndex) => {
+          const key = `visibility-toggle-${columnIndex}`;
+
+          return (
+            <label
+              className="visibility-label"
+              htmlFor={key}
+              style={visibilityStyles.label}
+              key={key}
+            >
+              <span
+                className="visibility-value"
+                style={visibilityStyles.value}
+              >
+                {label}
+              </span>
+              <input
+                className="visibility-toggle"
+                type="checkbox"
+                id={key}
+                style={visibilityStyles.toggle}
+                checked={visible}
+                onChange={() => onToggleColumn(columnIndex)}
+              />
+            </label>
+          );
+        }
       )}
     </div>
   );
 };
 VisibilityToggles.propTypes = {
   columns: React.PropTypes.array,
-  onToggleColumn: React.PropTypes.func
+  onToggleColumn: React.PropTypes.func,
+  styles: React.PropTypes.object
 };
 
 export default VisibilityToggles;
