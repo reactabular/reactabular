547787853312c281cefff00613ac9a42333a0fdc
Juho Vepsalainen <bebraw@gmail.com>
2016-08-15 10:59:08 +0300

    docs - Fix default column definition

--- docs/data/definitions.jsx
+++ docs/data/definitions.jsx
@@ -15,19 +15,15 @@ const rows = [
 
 const columns = [
   {
+    property: 'name',
     header: {
       label: 'Name'
-    },
-    cell: {
-      property: 'name'
     }
   },
   {
+    property: 'dad',
     header: {
       label: 'Dad'
-    },
-    cell: {
-      property: 'dad'
     }
   }
 ];
