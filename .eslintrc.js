052b9e5d95a89da6eda56f568cfa14bb32bc5997
Juho Vepsalainen <bebraw@gmail.com>
2016-09-25 14:41:08 +0300

    eslint - Document disabled rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,15 +10,15 @@ module.exports = {
     "react"
   ],
   "rules": {
-    "comma-dangle": ["error", "never"],
-    "prefer-arrow-callback": 0,
-    "func-names": 0,
-    "import/no-extraneous-dependencies": 0,
-    "import/no-unresolved": [1, { ignore: ['^reactabular'] }],
-    "no-underscore-dangle": 0,
-    "no-unused-expressions": 0,
-    "no-use-before-define": 0,
-    "react/sort-comp": 0,
-    "react/no-multi-comp": 0
+    "comma-dangle": ["error", "never"], // personal preference
+    "prefer-arrow-callback": 0, // mocha tests (recommendation)
+    "func-names": 0, // mocha tests (recommendation)
+    "import/no-extraneous-dependencies": 0, // monorepo setup
+    "import/no-unresolved": [1, { ignore: ['^reactabular'] }], // monorepo setup
+    "no-underscore-dangle": 0, // implementation detail (_highlights etc.)
+    "no-unused-expressions": 0, // chai
+    "no-use-before-define": 0, // personal preference
+    "react/sort-comp": 0, // personal preference
+    "react/no-multi-comp": 0 // personal preference
   }
 };
