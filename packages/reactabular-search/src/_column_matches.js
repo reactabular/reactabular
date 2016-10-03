b463c4457c4fb6ffc688f7b4f8e9601f5e105ff6
Mathieu M-Gosselin <mathieumg@gmail.com>
2016-10-03 10:34:36 -0400

    Introduced the `castingStrategy` parameter to `_columnMatches`.

--- packages/reactabular-search/src/_column_matches.js
+++ packages/reactabular-search/src/_column_matches.js
@@ -2,6 +2,7 @@ import strategies from './strategies';
 
 const _columnMatches = ({
   query,
+  castingStrategy = v => (Array.isArray(v) ? v : String(v)),
   column = {},
   row,
   strategy = strategies.infix,
@@ -9,7 +10,7 @@ const _columnMatches = ({
 }) => {
   const property = column.property;
   // Pick resolved value by convention
-  const resolvedValue = String(row[`_${property}`] || row[property]);
+  const resolvedValue = castingStrategy(row[`_${property}`] || row[property]);
 
   return strategy(transform(query)).evaluate(transform(resolvedValue));
 };
