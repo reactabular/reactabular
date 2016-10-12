db1cfc1e00ee14deadc7c9a1ae8bbdfa55dcdac7
Juho Vepsalainen <bebraw@gmail.com>
2016-10-12 15:26:53 +0300

    tree - Rewrite `tree.unpack` in a terser form

--- packages/reactabular-tree/src/unpack.js
+++ packages/reactabular-tree/src/unpack.js
@@ -1,38 +1,36 @@
 import omit from 'lodash/omit';
 
-function unpack({
+const unpack = ({
   parentField = 'parent',
   childrenField = 'children',
   idField = 'id',
   parent
-} = {}) {
-  return (rows) => {
-    if (!Array.isArray(rows)) {
-      return [];
-    }
+} = {}) => (rows) => {
+  if (!Array.isArray(rows)) {
+    return [];
+  }
 
-    if (!rows.length) {
-      return rows;
-    }
+  if (!rows.length) {
+    return rows;
+  }
 
-    return [].concat(
-      ...rows.map(node => {
-        const children = node[childrenField];
-        const d = parent ? {
-          ...omit(node, childrenField),
-          [parentField]: parent
-        } : omit(node, childrenField);
+  return [].concat(
+    ...rows.map((node) => {
+      const children = node[childrenField];
+      const d = parent ? {
+        ...omit(node, childrenField),
+        [parentField]: parent
+      } : omit(node, childrenField);
 
-        return [d].concat(
-          unpack({
-            parentField,
-            parent: d[idField],
-            idField
-          })(children)
-        );
-      })
-    );
-  };
-}
+      return [d].concat(
+        unpack({
+          parentField,
+          parent: d[idField],
+          idField
+        })(children)
+      );
+    })
+  );
+};
 
 export default unpack;
