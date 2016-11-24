372155d63f9db260b9894d4c02dffed1c565fe58
Juho Vepsalainen <bebraw@gmail.com>
2016-11-24 18:17:03 +0200

    chore(sort) - Use kebabcase for file naming

--- packages/reactabular-sort/src/index.js
+++ packages/reactabular-sort/src/index.js
@@ -1,6 +1,6 @@
-export { default as byColumn } from './by_column';
-export { default as byColumns } from './by_columns';
-export { default as byColumnsPrioritizeLastSorted } from './by_columns_prioritize_last_sorted';
+export { default as byColumn } from './by-column';
+export { default as byColumns } from './by-columns';
+export { default as byColumnsPrioritizeLastSorted } from './by-columns-prioritize-last-sorted';
 export { default as header } from './header';
 export { default as sorter } from './sorter';
 export { default as reset } from './reset';
