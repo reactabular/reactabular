cc46929efaed565e990b23b6c578cfaf81a912ef
Artem Sapegin <artem@sapegin.ru>
2016-12-16 14:04:33 +0100

    fix: rowKey could be 0

--- packages/reactabular-table/src/resolve-row-key.js
+++ packages/reactabular-table/src/resolve-row-key.js
@@ -5,7 +5,7 @@ function resolveRowKey({ rowData, rowIndex, rowKey }) {
     return `${rowKey({ rowData, rowIndex })}-row`;
   } else if (process.env.NODE_ENV !== 'production') {
     // Arrays cannot have rowKeys by definition so we have to go by index there.
-    if (!isArray(rowData) && !rowData[rowKey]) {
+    if (!isArray(rowData) && rowData[rowKey] === undefined) {
       console.warn( // eslint-disable-line no-console
         'Table.Body - Missing valid rowKey!',
         rowData,
