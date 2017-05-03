c977bea96f74b1a7f2d37c176455ccbcc9db9804
José Diaz Seng <josediazseng@gmx.de>
2017-05-03 12:32:56 +0200

    Fixed react-dom/test-utils import

--- packages/reactabular-table/__tests__/body_test.jsx
+++ packages/reactabular-table/__tests__/body_test.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
+import TestUtils from 'react-dom/test-utils';
 import { expect } from 'chai';
 import * as edit from 'react-edit';
 import * as resolve from 'table-resolver';
