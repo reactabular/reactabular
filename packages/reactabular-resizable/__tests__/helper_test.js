7ac2a1af2f1b1a24f75620b9fda1eb63da862ba4
Junho Park <cnaa97@gmail.com>
2017-06-21 18:46:47 +0900

    #307 add test code to nested columns with resizing helper

--- packages/reactabular-resizable/__tests__/helper_test.js
+++ packages/reactabular-resizable/__tests__/helper_test.js
@@ -27,4 +27,90 @@ describe('helper', function () {
 
     expect(helper.initialize(columns)).toEqual(expected);
   });
+
+  it('With nested columns, class names only leaf column correctly', function () {
+    const column = {
+      property: 'demo1',
+      props: {
+        className: 'demo1'
+      },
+      header: {
+        label: 'Demo1'
+      }
+    };
+    const childrenColumn = {
+      property: 'boo',
+      props: {
+        className: 'boo'
+      },
+      header: {
+        label: 'Boo'
+      }
+    };
+    const column2 = {
+      property: 'demo2',
+      props: {
+        className: 'demo2'
+      },
+      header: {
+        label: 'Demo2'
+      },
+      children: [
+        {
+          property: 'cha',
+          props: {
+            className: 'cha'
+          },
+          header: {
+            label: 'cha'
+          }
+        },
+        childrenColumn
+      ]
+    };
+    const columns = [column, column2];
+    const expected = [
+      Object.assign(column, {
+        props: {
+          className: 'demo1 column-foo-bar'
+        }
+      }),
+      {
+        property: 'demo2',
+        props: {
+          className: 'demo2'
+        },
+        header: {
+          label: 'Demo2'
+        },
+        children: [
+          {
+            property: 'cha',
+            props: {
+              className: 'cha column-foo-bar'
+            },
+            header: {
+              label: 'cha'
+            }
+          },
+          {
+            property: 'boo',
+            props: {
+              className: 'boo column-foo-bar'
+            },
+            header: {
+              label: 'Boo'
+            }
+          }
+        ]
+      }
+    ];
+
+    const helper = resizableHelper({
+      globalId: 'foo',
+      getId: () => 'bar'
+    });
+
+    expect(helper.initialize(columns)).toEqual(expected);
+  });
 });
