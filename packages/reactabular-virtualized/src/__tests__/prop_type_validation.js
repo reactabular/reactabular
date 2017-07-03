4eec26574348cd381c4e05cf684ce03e7f6d2ffe
rnsloan <rnsloan1@gmail.com>
2017-07-04 04:11:25 +1000

    fix virtualization propType warning (#310)

---
+++ packages/reactabular-virtualized/src/__tests__/prop_type_validation.js
@@ -0,0 +1,33 @@
+import { heightPropCheck } from '../body.js';
+
+describe('height propType validator', function () {
+  it('has no error when height is a number', function () {
+    expect(
+      heightPropCheck({ height: 50 }, 'height', 'VirtualizedBody')
+    ).toBeUndefined();
+  });
+
+  it('has no error when style.maxHeight is a number', function () {
+    expect(
+      heightPropCheck({ style: { maxHeight: 50 } }, 'height', 'VirtualizedBody')
+    ).toBeUndefined();
+  });
+
+  it('has an error when height and style.maxHeight are not provided', function () {
+    expect(
+      heightPropCheck({}, 'height', 'VirtualizedBody')
+    ).toThrow();
+  });
+
+  it('has an error when height is not a number', function () {
+    expect(
+      heightPropCheck({ height: '50px' }, 'height', 'VirtualizedBody')
+    ).toThrow();
+  });
+
+  it('has an error when style.maxHeight is not a number', function () {
+    expect(
+      heightPropCheck({ style: { maxHeight: '50px' } }, 'height', 'VirtualizedBody')
+    ).toThrow();
+  });
+});
