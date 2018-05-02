d93527e1b5b4f3b1b68112b22faca7f39192a688
Juho Vepsalainen <bebraw@gmail.com>
2018-05-02 16:18:30 +0300

    chore: Create renderers through React.createElement
    This way class based components can be renderers too.

--- packages/table/src/header-row.js
+++ packages/table/src/header-row.js
@@ -3,20 +3,26 @@ import React from 'react';
 import { tableHeaderRowTypes, tableHeaderRowDefaults } from './types';
 import renderCell from './render-cell';
 
-const HeaderRow = ({
-  rowData, rowIndex, renderers
-}) => (
-  renderers.row({
-    rowIndex,
-    rowData,
-    renderer: renderers.row,
-    children: rowData.map((column, columnIndex) =>
+// eslint-disable-next-line react/prefer-stateless-function
+class HeaderRow extends React.Component {
+  render() {
+    const { rowData, rowIndex, renderers } = this.props;
+
+    return React.createElement(
+      renderers.row,
+      {
+        rowIndex,
+        rowData,
+        renderer: renderers.row
+      },
+      rowData.map((column, columnIndex) =>
       <React.Fragment key={`${columnIndex}-header-cell`}>{
         renderCell(columnIndex, renderers.cell, column, column.headerCell, column.headerCell)
       }</React.Fragment>
     )
-  })
-);
+    );
+  }
+}
 HeaderRow.defaultProps = tableHeaderRowDefaults;
 HeaderRow.propTypes = tableHeaderRowTypes;
 
