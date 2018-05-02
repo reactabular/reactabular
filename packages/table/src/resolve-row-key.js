c26ff3cd5c7c64441240e6106e73c4952258a1e9
Juho Vepsalainen <bebraw@gmail.com>
2018-05-02 08:54:21 +0300

    chore: Use Array.isArray over lodash alternative

--- packages/table/src/resolve-row-key.js
+++ packages/table/src/resolve-row-key.js
@@ -1,11 +1,9 @@
-import { isArray } from 'lodash';
-
 function resolveRowKey({ rowData, rowIndex, rowKey }) {
   if (typeof rowKey === 'function') {
     return `${rowKey({ rowData, rowIndex })}-row`;
   } else if (process.env.NODE_ENV !== 'production') {
     // Arrays cannot have rowKeys by definition so we have to go by index there.
-    if (!isArray(rowData) && rowData[rowKey] === undefined) {
+    if (!Array.isArray(rowData) && rowData[rowKey] === undefined) {
       console.warn( // eslint-disable-line no-console
         'Table.Body - Missing valid rowKey!',
         rowData,
